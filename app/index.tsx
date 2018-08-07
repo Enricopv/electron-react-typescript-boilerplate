1eefb465947a49965d817062a3e139899f9f4d52
Enrico Valbuena <enricovalbuena@yahoo.com>
2018-08-06 18:20:00 -0700

    Redux is gone

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,8 @@
 import * as React from 'react';
 import { render } from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
-// import Root from './containers/Root';
 import './app.global.scss';
 import Routes from "./routes"
-
-// const { configureStore, history } = require('./store/configureStore');
-// const store = configureStore();
 
 render(
   <AppContainer>
